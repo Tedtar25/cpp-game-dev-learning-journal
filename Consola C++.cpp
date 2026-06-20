34b9d05381eee9b4c6ff9ae7d62b75334977958e
Tedtar25 <cesar.carrillo69@hotmail.com>
2026-06-20 12:58:45 -0600

    Add project files.

---
+++ Consola C++.cpp
@@ -0,0 +1,91 @@
+// Consola C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+#include <iostream>
+
+
+// #####################################################
+// ##CAPITULO 1 - CAPITULO 1 - CAPITULO 1 - CAPITULO 1##
+// #####################################################
+
+//int main() {
+    //std::cout << "Hola, mundo. Estoy aprendiendo C++ para videojuegos." << std::endl;
+    //return 0;
+//}
+
+/*
+#include <iostream>
+
+int main() {
+    std::cout << "=================" << std::endl;
+    std::cout << "     MINI RPG    " << std::endl;
+    std::cout << "=================" << std::endl;
+
+    std::cout << "Jugador creado correctamente" << std::endl;
+    std::cout << "Vida inicial: 100" << std::endl;
+    std::cout << "Ataque inicial: 15" << std::endl;
+
+    return 0;
+}
+*/
+
+// EJERCICIO 1 - Imprime tu propio mensaje:
+/*
+int main() {
+    std::cout << "Hola soy Cesar y estoy aprendiendo C++" << std::endl;
+    
+    return 0;
+}
+
+//EJERCICIO 2 - Haz una pantalla de inicio para un juego inventado.
+int main() {
+    std::cout << "===============================" << std::endl;
+    std::cout << "         LONELY SPACE          " << std::endl;
+    std::cout << "===============================" << std::endl;
+    std::cout << "Presiona ENTER para iniciar...." << std::endl;
+
+    return 0;
+}
+
+//EJERCICIO 3 - Imprime estadísticas de un personaje:
+int main() {
+    std::cout << "Nombre: Mago" << std::endl;
+    std::cout << "HP: 100" << std::endl;
+    std::cout << "Ataque: 20" << std::endl;
+    std::cout << "Defensa: 10" << std::endl;
+
+    return 0;
+}
+*/
+
+/*
+//Mini reto del capítulo
+int main() {
+    std::cout << "==============================" << std::endl;
+    std::cout << "         DUNGEON C++          " << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "1. Nueva Partida" << std::endl;
+    std::cout << "2. Cargar Partida" << std::endl;
+    std::cout << "3. Opciones" << std::endl;
+    std::cout << "4. Salir" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "Selecciona una opcion:" << std::endl;
+
+    return 0;
+}
+*/
+
+
